5212fd4c9ada875a71446cb927c5cae70b886b5c
namaht123 <133759843+namaht123@users.noreply.github.com>
2023-11-25 21:43:32 +0700

    Add files via upload

--- app/src/main/java/com/example/movie_application/Activities/DetailActivity.java
+++ app/src/main/java/com/example/movie_application/Activities/DetailActivity.java
@@ -1,82 +1,86 @@
-package com.example.movie_application.Activities;
+package huanvdph35061.fpoly.moviestime;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.movie_application.R;
-import com.google.gson.Gson;
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import huanvdph35061.fpoly.moviestime.Adapter.CastAdapter;
+import huanvdph35061.fpoly.moviestime.model.Cast;
 
 public class DetailActivity extends AppCompatActivity {
-     private RequestQueue mRequestQueue;
-     private StringRequest mStringRequest;
-     private ProgressBar progressBar;
-     private TextView titleTxt,movieRateTxt, movieTimeTxt, movieSummaryInfo, movieActorInfo;
-     private int idFila;
-     private ImageView pic2, backImg;
-     private RecyclerView.Adapter adapterActorList, adapterCategory;
-     private RecyclerView recyclerViewActor, recyclerViewCategory;
-     private NestedScrollView scrollView;
+
+    private ImageView MovieThumbImg,MovieCoverImg;
+    private String url;
+
+    private TextView txtTitle, txtDes;
+
+    private FloatingActionButton play_fab;
+    CastAdapter castAdapter;
+    private RecyclerView recyclerCast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        recyclerCast = findViewById(R.id.RcvCast);
 
-        idFila = getIntent().getIntExtra("id",0);
-        initView();
-        sendRequest();
+        iniViews();
+        setupRvCast();
     }
+    private void iniViews(){
+        play_fab = findViewById(R.id.play_fab);
+        String movieTitle = getIntent().getExtras().getString("title");
+        int imageResourceId = getIntent().getExtras().getInt("imgURL");
+        int imageCover = getIntent().getExtras().getInt("imgCover");
+        String movieDes = getIntent().getExtras().getString("desc");
+        MovieThumbImg = findViewById(R.id.detail_movie_img);
+        MovieThumbImg.setImageResource(imageResourceId);
+        MovieCoverImg = findViewById(R.id.detail_movie_cover);
+        Glide.with(this).load(imageCover).into(MovieCoverImg);
+        txtTitle = findViewById(R.id.detail_movie_title);
+        txtTitle.setText(movieTitle);
+        txtDes = findViewById(R.id.detail_movie_desc);
+        txtDes.setText(movieDes);
+        url = getIntent().getStringExtra("url");
 
-    private void sendRequest() {
-        mRequestQueue = Volley.newRequestQueue(this);
-        progressBar.setVisibility(View.VISIBLE);
-        scrollView.setVisibility(View.GONE);
-        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies/" + idFila, new Response.Listener<String>() {
+        MovieCoverImg.setAnimation(AnimationUtils.loadAnimation(this,R.anim.scale_animation));
+        play_fab.setAnimation(AnimationUtils.loadAnimation(this,R.anim.scale_animation));
+
+        play_fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(String response) {
-                Gson gson = new Gson();
-                progressBar.setVisibility(View.GONE);
-                scrollView.setVisibility(View.VISIBLE);
-                
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, VideoPlayerActivity.class);
+                intent.putExtra("url",url);
+                startActivity(intent);
             }
         });
-        mRequestQueue.add(mStringRequest);
-    }
 
-    private void initView() {
-        titleTxt = findViewById(R.id.movieNameTxt);
-        progressBar = findViewById(R.id.progressBarDetail);
-        scrollView = findViewById(R.id.scrollView3);
-        pic2 = findViewById(R.id.picDetail);
-        movieRateTxt = findViewById(R.id.movieStar);
-        movieTimeTxt = findViewById(R.id.movieTime);
-        movieSummaryInfo = findViewById(R.id.movieSummery);
-        movieActorInfo = findViewById(R.id.movieActor);
-        backImg = findViewById(R.id.backImg);
-        recyclerViewCategory = findViewById(R.id.genreView);
-        recyclerViewActor = findViewById(R.id.imagetRecyview);
-        recyclerViewActor.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        recyclerViewCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
-        backImg.setOnClickListener(v -> finish());
+    }
+    private void setupRvCast(){
+        List<Cast> mData = new ArrayList<>();
+        mData.add(new Cast("name",R.drawable.cast1));
+        mData.add(new Cast("name",R.drawable.cast2));
+        mData.add(new Cast("name",R.drawable.cast3));
+        mData.add(new Cast("name",R.drawable.cast4));
+        mData.add(new Cast("name",R.drawable.cast5));
+        castAdapter = new CastAdapter(this,mData);
+        recyclerCast .setAdapter(castAdapter);
+        recyclerCast.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
     }
 }
--- app/src/main/java/com/example/movie_application/Activities/MainActivity.java
+++ app/src/main/java/com/example/movie_application/Activities/MainActivity.java
@@ -1,171 +1,184 @@
-package com.example.movie_application.Activities;
+package huanvdph35061.fpoly.moviestime;
 
-import androidx.annotation.NonNull;
+import static huanvdph35061.fpoly.moviestime.utils.DataSource.getPhimMoi;
+
+import android.app.ActivityOptions;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+import androidx.appcompat.widget.SearchView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.CompositePageTransformer;
-import androidx.viewpager2.widget.MarginPageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager.widget.ViewPager;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.movie_application.Adapters.CategoryListAdapter;
-import com.example.movie_application.Adapters.FilmListAdapter;
-import com.example.movie_application.Adapters.SliderAdapters;
-import com.example.movie_application.Domain.GenresItem;
-import com.example.movie_application.Domain.ListFilm;
-import com.example.movie_application.Domain.SliderItems;
-import com.example.movie_application.R;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView.Adapter adapterBestMovies, adapterUpcomming, adapterCategory;
-    private RecyclerView recyclerViewBestMovies, recyclerViewUpComming, recyclerViewCategory;
-    private RequestQueue mRequestQueue;
-    private StringRequest mStringRequest, mStringRequest2, mStringRequest3;
-    private ProgressBar loading1, loading2, loading3;
-    private ViewPager2 viewPager2;
-    private Handler slideHandler = new Handler();
+import huanvdph35061.fpoly.moviestime.Adapter.MovieAdapter;
+import huanvdph35061.fpoly.moviestime.Adapter.SearchAdapter;
+import huanvdph35061.fpoly.moviestime.Adapter.SliderAdapter;
+import huanvdph35061.fpoly.moviestime.model.Movie;
+import huanvdph35061.fpoly.moviestime.model.MovieItemClickListener;
+import huanvdph35061.fpoly.moviestime.model.Slide;
+import huanvdph35061.fpoly.moviestime.utils.DataSource;
+
+
+public class MainActivity extends AppCompatActivity implements MovieItemClickListener {
+
+    private ViewPager slidePager;
+    private TabLayout indicator;
+
+    private RecyclerView RvPhimMoi,RvPhimKinhDi,RvPhimAnime;
+
+    private TabLayout categoryTab;
+
+    List<Slide> homeBannerList;
+
+    private Toolbar toolbar;
+
+    private SearchAdapter searchAdapter;
+
+    private SearchView searchView;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        slidePager = findViewById(R.id.slide_pager);
+        indicator = findViewById(R.id.indicator);
+        RvPhimMoi = findViewById(R.id.RcPhimMoi);
+        RvPhimKinhDi = findViewById(R.id.RcPhimKinhDi);
+        RvPhimAnime = findViewById(R.id.RcPhimAnime);
+        toolbar = findViewById(R.id.toolbar);
 
-        initView();
-        banner();
-        sendRequestBestMovies();
-        sendRequestUpComming();
-        sendRequestCategory();
+
+        setSupportActionBar(toolbar);
+
+
+
+
+        iniSlider();
+
+        iniPhimMoi();
+        iniPhimKinhDi();
+        iniPhimAnime();
+
     }
 
-    private void sendRequestBestMovies() {
-        mRequestQueue = Volley.newRequestQueue(this);
-        loading1.setVisibility(View.VISIBLE);
-        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=1", response -> {
-            Gson gson = new Gson();
-            loading1.setVisibility(View.GONE);
-            ListFilm items = gson.fromJson(response, ListFilm.class);
-            adapterBestMovies = new FilmListAdapter(items);
-            recyclerViewBestMovies.setAdapter(adapterBestMovies);
+    private void iniPhimMoi() {
 
-        }, error -> {
-            loading1.setVisibility(View.GONE);
-            Log.i("UiLover", "onErrorResponse: "+ error.toString());
-
-        });
-        mRequestQueue.add(mStringRequest);
-    }
-    private void sendRequestUpComming() {
-        mRequestQueue = Volley.newRequestQueue(this);
-        loading3.setVisibility(View.VISIBLE);
-        mStringRequest3 = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=2", response -> {
-            Gson gson = new Gson();
-            loading3.setVisibility(View.GONE);
-            ListFilm items = gson.fromJson(response, ListFilm.class);
-            adapterUpcomming = new FilmListAdapter(items);
-            recyclerViewUpComming.setAdapter(adapterUpcomming);
-
-        }, error -> {
-            loading3.setVisibility(View.GONE);
-            Log.i("UiLover", "onErrorResponse: "+ error.toString());
-
-        });
-        mRequestQueue.add(mStringRequest3);
-    }
-    private void sendRequestCategory() {
-        mRequestQueue = Volley.newRequestQueue(this);
-        loading2.setVisibility(View.VISIBLE);
-        mStringRequest2 = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/genres", response -> {
-            Gson gson = new Gson();
-            loading2.setVisibility(View.GONE);
-            ArrayList<GenresItem> catList = gson.fromJson(response, new TypeToken<ArrayList<GenresItem>>(){
-            }.getType());
-            adapterCategory = new CategoryListAdapter(catList);
-            recyclerViewCategory.setAdapter(adapterCategory);
-
-        }, error -> {
-            loading2.setVisibility(View.GONE);
-            Log.i("UiLover", "onErrorResponse: "+ error.toString());
-
-        });
-        mRequestQueue.add(mStringRequest2);
+        MovieAdapter movieAdapter = new MovieAdapter(this, getPhimMoi(),this);
+        RvPhimMoi.setAdapter(movieAdapter);
+        RvPhimMoi.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
     }
 
-    private void banner() {
-        List<SliderItems> sliderItems = new ArrayList<>();
-        sliderItems.add(new SliderItems(R.drawable.wide));
-        sliderItems.add(new SliderItems(R.drawable.wide1));
-        sliderItems.add(new SliderItems(R.drawable.wide3));
+    private void iniPhimKinhDi(){
+        MovieAdapter movieAdapter = new MovieAdapter(this, DataSource.getPhimKinhDi(),this);
+        RvPhimKinhDi.setAdapter(movieAdapter);
+        RvPhimKinhDi.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+    }
+    private void iniPhimAnime(){
+        MovieAdapter movieAdapter = new MovieAdapter(this, DataSource.getPhimAnime(),this);
+        RvPhimAnime.setAdapter(movieAdapter);
+        RvPhimAnime.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+    }
 
-        viewPager2.setAdapter(new SliderAdapters(sliderItems,viewPager2));
-        viewPager2.setClipToPadding(false);
-        viewPager2.setOffscreenPageLimit(3);
-        viewPager2.setClipChildren(false);
-        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+    private void iniSearch(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        RvPhimMoi.setLayoutManager(linearLayoutManager);
 
-        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
-        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+        searchAdapter = new SearchAdapter(getPhimMoi());
+        RvPhimMoi.setAdapter(searchAdapter);
+
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        RvPhimMoi.addItemDecoration(itemDecoration);
+    }
+
+
+    private void iniSlider() {
+        homeBannerList = new ArrayList<>();
+        homeBannerList.add(new Slide(R.drawable.slide1,"Slide Title / more text here"));
+        homeBannerList.add(new Slide(R.drawable.slide2,"Slide Title / more text here"));
+        homeBannerList.add(new Slide(R.drawable.slide1,"Slide Title / more text here"));
+        homeBannerList.add(new Slide(R.drawable.slide2,"Slide Title / more text here"));
+
+        SliderAdapter adapter = new SliderAdapter(this,homeBannerList);
+        slidePager.setAdapter(adapter);
+
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new SliderTime(),4000,6000);
+        indicator.setupWithViewPager(slidePager,true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void transformPage(@NonNull View page, float position) {
-                float r =1-Math.abs(position);
-                page.setScaleY(0.85f+r*0.15f);
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
-        viewPager2.setPageTransformer(compositePageTransformer);
-        viewPager2.setCurrentItem(1);
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                slideHandler.removeCallbacks(sliderRunnable);
-            }
-        });
+
+        return true;
     }
-    private Runnable sliderRunnable = new Runnable() {
+
+    @Override
+    public void onMovieClick(Movie movie, ImageView movieImageView) {
+        Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+        intent.putExtra("title",movie.getTitle());
+        intent.putExtra("imgURL",movie.getThumb());
+        intent.putExtra("imgCover",movie.getCoverPhoto());
+        intent.putExtra("desc",movie.getDes());
+        intent.putExtra("url",movie.getStreamLink());
+
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,movieImageView,"sharedName");
+        startActivity(intent,options.toBundle());
+
+    }
+
+    class SliderTime extends TimerTask{
+
         @Override
         public void run() {
-            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (slidePager.getCurrentItem()<homeBannerList.size()-1){
+                        slidePager.setCurrentItem(slidePager.getCurrentItem()+1);
+                    }else {
+                        slidePager.setCurrentItem(0);
+                    }
+                }
+            });
         }
-    };
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        slideHandler.removeCallbacks(sliderRunnable);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        slideHandler.postDelayed(sliderRunnable, 2000);
-    }
-
-    private void initView() {
-        viewPager2 = findViewById(R.id.viewpagerSlide);
-        recyclerViewBestMovies = findViewById(R.id.view1);
-        recyclerViewBestMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        recyclerViewUpComming = findViewById(R.id.view3);
-        recyclerViewUpComming.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        recyclerViewCategory = findViewById(R.id.view2);
-        recyclerViewCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        loading1 = findViewById(R.id.progressBar1);
-        loading2 = findViewById(R.id.progressBar2);
-        loading3 = findViewById(R.id.progressBar3);
-
     }
 }
